30243150bbe5d34ab4a1e8179f2a1526affeafee
Miroljub <miroljub1995@gmail.com>
2020-03-01 12:08:22 +0100

    do not use global

--- src/node_worker.ts
+++ src/node_worker.ts
@@ -2,22 +2,5 @@ import { runInThisContext } from 'vm'
 import { parentPort, workerData } from 'worker_threads'
 import * as m from 'module'
 
-declare global {
-    namespace NodeJS {
-        interface Global {
-            port: {},
-            f: {}
-        }
-    }
-}
-
-global.port = parentPort;
-global.f = workerData.f;
-const getPort = () => global.port;
-
-// const f1 = (p: any) => { global = undefined; return f(p); }
-
-const funDef = `const f=${global.f.toString()};`;
-const work = `(${getPort.toString()})().on('message',m=>(${getPort.toString()})().postMessage(f(m)));`;
-const code = funDef + work;
-runInThisContext(m.wrap(code))(exports, require, module, __filename, __dirname);
+const functionF = runInThisContext(m.wrap(`return ${workerData.f.toString()};`))(exports, require, module, __filename, __dirname);
+parentPort.on('message', param => parentPort.postMessage(functionF(param)));
